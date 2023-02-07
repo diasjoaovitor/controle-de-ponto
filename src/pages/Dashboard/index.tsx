f88d15cd6b3b48b25cfcd4aa95a5ef4695b70183
João VItor <jvitordiass@outlook.com>
2023-02-07 14:40:25 -0300

    feat: MUI DataGrid

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,18 @@
-import { AppBar } from '../../shared/components'
+import { DataGrid } from '@mui/x-data-grid'
+import { Layout } from '../../shared/components'
+import { employees } from '../../repositories'
+import { columns, localeText } from './data-grid'
 
 export const Dashboard: React.FC = () => {
   return (
-    <AppBar />
+    <Layout page="Dashboard">
+      <DataGrid
+        rows={employees} columns={columns}
+        autoHeight
+        rowsPerPageOptions={[10]}
+        disableSelectionOnClick
+        localeText={localeText}
+      />
+    </Layout>
   )
 }
