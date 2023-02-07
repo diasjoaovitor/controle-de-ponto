8723df1c1fdeba1831134a5a2054767185a104c4
João VItor <jvitordiass@outlook.com>
2023-02-07 14:43:45 -0300

    env: fake employees

---
+++ src/repositories/index.ts
@@ -0,0 +1,32 @@
+export const employees = [
+  {
+    fullName: 'AGDA BRITO DO ESPIRITO SANTO', 
+    cpf: '123.456.789-10',
+    role: 'OP DE CAIXA',
+    id: 1
+  },
+  {
+    fullName: 'AGNALDO FRANCISCO DA CRUZ FILHO', 
+    cpf: '123.456.789-10',
+    role: 'REPOSITOR',
+    id: 2
+  },
+  {
+    fullName: 'AKIRIS SANTANA LEMOS', 
+    cpf: '123.456.789-10',
+    role: 'ATENDENTE DE BALCÃO',
+    id: 3
+  },
+  {
+    fullName: 'AGDA BRITO DO ESPIRITO SANTO', 
+    cpf: '123.456.789-10',
+    role: 'OP DE CAIXA',
+    id: 4
+  },
+  {
+    fullName: 'AGDA BRITO DO ESPIRITO SANTO', 
+    cpf: '123.456.789-10',
+    role: 'OP DE CAIXA',
+    id: 5
+  }
+]
